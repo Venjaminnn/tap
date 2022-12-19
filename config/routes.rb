ac709cff4885ea04aca6f2f7f34fcfecd267cf7b
Venjaminnn <victoriagalchenko@gmail.com>
2022-12-19 11:03:12 +0300

    Added factory posts, updating for posts, route for update

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :posts, only: :create
+  resources :posts, only: %i[create update]
 end
