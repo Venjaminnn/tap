da0b07bd3fd2a393e41f0352626655b588647786
Venjaminnn <victoriagalchenko@gmail.com>
2023-01-04 18:14:23 +0300

    Added create comment migration and table, comment model and route,  comments controller and spec for it

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:current_post) { create(:post, user: user) }
+
+  describe 'POST #create' do
+    context 'when successfully creating comment' do
+      let(:text) { 'one random comment' }
+
+      let(:params) do
+        {
+          text: 'my new comment',
+          post_id: current_post.id
+        }
+      end
+
+      before do
+        session[:user_id] = user.id
+      end
+
+      it 'create comment from current user' do
+        post :create, params: params
+
+        expect(JSON.parse(response.body).symbolize_keys).to include(text: 'my new comment',
+                                                                    user_id: user.id,
+                                                                    post_id: current_post.id)
+      end
+    end
+
+    context 'when creating comment failed' do
+      before do
+        session[:user_id] = user.id
+
+        post :create, params: params
+      end
+
+      let(:params) do
+        {
+          post_id: current_post.id
+        }
+      end
+
+      it 'create comment from current user' do
+        expect(JSON.parse(response.body).symbolize_keys).to eq(text: ['can\'t be blank'])
+      end
+    end
+
+    context 'when trying to create comment without log in' do
+      before do
+        session[:user_id] = nil
+
+        post :create, params: params
+      end
+
+      let(:params) do
+        {
+          text: 'my new comment',
+          post_id: current_post.id
+        }
+      end
+
+      it 'user is not logged' do
+        post :create, params: params
+
+        expect(response.body).to eq('User is not logged')
+      end
+    end
+  end
+end
